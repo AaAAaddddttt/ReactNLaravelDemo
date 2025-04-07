9cd8c231acaf89a6293393343881572ceccba49a
unknown <biopro114@gmail.comgit config --global user.name AaAAaddddtttgit commit -m first>
2025-04-08 02:31:50 +0800

    First commit

---
+++ server/app/Models/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $table = 'tbl_users';
+    protected $primaryKey = 'user_id';
+    protected $fillable = [
+        'first_name',
+        'middle_name',
+        'last_name',
+        'suffix_name',
+        'age',
+        'gender_id',
+        'address'
+    ];
+}
